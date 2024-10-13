abc5cf0701802468c79a8c6ce084c1e7e98cd2ec
dongcao0307 <caothanhdong.41118@gmail.com>
2024-10-13 23:26:50 +0700

    add NhanVien

--- src/entity/NhanVien.java
+++ src/entity/NhanVien.java
@@ -1,5 +1,124 @@
 package entity;
 
-public class NhanVien {
+import java.util.Date;
+import java.util.Objects;
 
+public class NhanVien {
+    private String maNV;
+    private String hoTen;
+    private int chucVu;
+    private int soDT;
+    private String diaChi;
+    private String email;
+    private Date ngaySinh;
+    private Date ngayVaoLam;
+    private double luongCoBan;
+    private double heSoLuong;
+    public NhanVien(String maNV, String hoTen, int chucVu, int soDT, String diaChi, String email, Date ngaySinh, Date ngayVaoLam, double luongCoBan, double heSoLuong) {
+        this.maNV = maNV;
+        this.hoTen = hoTen;
+        this.chucVu = chucVu;
+        this.soDT = soDT;
+        this.diaChi = diaChi;
+        this.email = email;
+        this.ngaySinh = ngaySinh;
+        this.ngayVaoLam = ngayVaoLam;
+        this.luongCoBan = luongCoBan;
+        this.heSoLuong = heSoLuong;
+    }
+
+    public NhanVien(String maNV) {
+        this(maNV, "hoTen", 0, 0, "diaChi", "email", new Date(), new Date(), 0.0, 0.0);
+    }
+    public String getMaNV() {
+        return maNV;
+    }
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public int getChucVu() {
+        return chucVu;
+    }
+
+    public void setChucVu(int chucVu) {
+        this.chucVu = chucVu;
+    }
+
+    public int getSoDT() {
+        return soDT;
+    }
+
+    public void setSoDT(int soDT) {
+        this.soDT = soDT;
+    }
+
+    public String getDiaChi() {
+        return diaChi;
+    }
+
+    public void setDiaChi(String diaChi) {
+        this.diaChi = diaChi;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getNgaySinh() {
+        return ngaySinh;
+    }
+
+    public void setNgaySinh(Date ngaySinh) {
+        this.ngaySinh = ngaySinh;
+    }
+
+    public Date getNgayVaoLam() {
+        return ngayVaoLam;
+    }
+
+    public void setNgayVaoLam(Date ngayVaoLam) {
+        this.ngayVaoLam = ngayVaoLam;
+    }
+
+    public double getLuongCoBan() {
+        return luongCoBan;
+    }
+
+    public void setLuongCoBan(double luongCoBan) {
+        this.luongCoBan = luongCoBan;
+    }
+
+    public double getHeSoLuong() {
+        return heSoLuong;
+    }
+
+    public void setHeSoLuong(double heSoLuong) {
+        this.heSoLuong = heSoLuong;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        NhanVien nhanVien = (NhanVien) o;
+        return Objects.equals(maNV, nhanVien.maNV);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(maNV);
+    }
 }

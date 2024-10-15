fa0fe2928d321853e3bb3dafc50563c016d8a380
tranvanhau04 <156154988+tranvanhau04@users.noreply.github.com>
2024-10-15 23:28:34 +0700

    Add KhachHang_DAO.java

--- src/dao/KhachHang_DAO.java
+++ src/dao/KhachHang_DAO.java
@@ -1,55 +1,112 @@
 package dao;
 
-import java.util.ArrayList;
-
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import connectDB.ConnectDB;
 import entity.KhachHang;
+import java.util.*;
+import java.sql.PreparedStatement;
 
 public class KhachHang_DAO {
-private ArrayList<KhachHang> dsKH;
-
+ private ArrayList<KhachHang> dsKhachHang;
+ private KhachHang khachHang;
 public KhachHang_DAO() {
-    dsKH = new ArrayList<KhachHang> (); 
+    dsKhachHang = new ArrayList<KhachHang>();
+    khachHang = new KhachHang();
 }
-
-public boolean themKH(KhachHang kh) {
-if(dsKH.contains(kh)) {
-    return false;
-}
- dsKH.add(kh);
- return true;
-}
-public boolean xoaKH(String maKH){
-    KhachHang kh = new KhachHang(maKH);
-    if(dsKH.contains(kh)) {
-        dsKH.remove(maKH);
-        return true;
-    } return false;
-}
-public KhachHang timKiem(String maKH){
-    KhachHang kh = new KhachHang(maKH);
-    if(dsKH.contains(maKH)){
-        return dsKH.get(dsKH.indexOf(maKH));
-    } return null;
-}
-
-public boolean suaKH(String maKH, KhachHang khNew){
-    KhachHang khachHang = new KhachHang(maKH);
-    if(dsKH.contains(khachHang)){
-        dsKH.set(dsKH.indexOf(khachHang), khNew);
-        khachHang.setHoTen(khNew.getHoTen());
-        khachHang.setDiaChi(khNew.getDiaChi());
-        khachHang.setSdt(khNew.getSdt());
-        khachHang.setEmail(khNew.getEmail());
-        khachHang.setcCCD(khNew.getcCCD());
-        return true;
-    } return false;
-}
-public ArrayList<KhachHang> layDSKH(){
-    return dsKH;
-}
-public KhachHang GetKH(int i){
-    if(i>=0 && i<dsKH.size()){
-        return dsKH.get(i);
-    } return null;
-}
+public ArrayList<KhachHang> getDsKhachHang() {
+    try {
+        Connection con = ConnectDB.getInstance().getConnection();
+        String sql = "select * from KhachHang";
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery(sql);
+        while (rs.next()) {
+            String maKH = rs.getString(1);
+            String hoTen = rs.getString(2);
+            String diaChi = rs.getString(3);
+            String sdt = rs.getString(4);
+            String email = rs.getString(5);
+            String cCCD = rs.getString(6);
+            KhachHang kh = new KhachHang(maKH, hoTen, diaChi, sdt, email, cCCD);
+            dsKhachHang.add(kh);
+        }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return dsKhachHang;
+    }
+public boolean createKhachHang(KhachHang kh) {
+    ConnectDB.getInstance();
+    Connection con = ConnectDB.getConnection();
+    PreparedStatement ps = null;
+    int n = 0;
+    try{
+        ps = con.prepareStatement("insert into" + " KhachHang values(?,?,?,?,?,?)");
+        ps.setString(1, kh.getMaKH());
+        ps.setString(2, kh.getHoTen());
+        ps.setString(3, kh.getDiaChi());
+        ps.setString(4, kh.getSdt());
+        ps.setString(5, kh.getEmail());
+        ps.setString(6, kh.getcCCD());
+        n = ps.executeUpdate();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return n > 0;
+    }
+public boolean updateKhachHang(KhachHang kh) {
+    ConnectDB.getInstance();
+    Connection con = ConnectDB.getConnection();
+    PreparedStatement ps = null;
+    int n = 0;
+    try{
+        ps = con.prepareStatement("update KhachHang set HoTen = ?, DiaChi = ?, SDT = ?, Email = ?, CCCD = ? where MaKH = ?");
+        ps.setString(1, kh.getHoTen());
+        ps.setString(2, kh.getDiaChi());
+        ps.setString(3, kh.getSdt());
+        ps.setString(4, kh.getEmail());
+        ps.setString(5, kh.getcCCD());
+        ps.setString(6, kh.getMaKH());
+        n = ps.executeUpdate();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return n > 0;
+    }
+public boolean deleteKhachHang(String maKH) {
+    ConnectDB.getInstance().connect();
+    PreparedStatement ps = null;
+    int n = 0;
+    try{
+        ps = ConnectDB.getConnection().prepareStatement("delete from KhachHang where MaKH = ?");
+        ps.setString(1, maKH);
+        n = ps.executeUpdate();
+    } catch (SQLException e) {
+        e.printStackTrace();
+    }
+    return n > 0;
+    }
+public KhachHang searchKhachHang(String maKH) {
+        ConnectDB.getInstance().connect();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try{
+            ps = ConnectDB.getConnection().prepareStatement("select * from KhachHang where MaKH = ?");
+            ps.setString(1, maKH);
+            rs = ps.executeQuery();
+            while (rs.next()) {
+                String hoTen = rs.getString(2);
+                String diaChi = rs.getString(3);
+                String sdt = rs.getString(4);
+                String email = rs.getString(5);
+                String cCCD = rs.getString(6);
+                khachHang = new KhachHang(maKH, hoTen, diaChi, sdt, email, cCCD);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return khachHang;
+    }
 }

10e4154ccbac48cc4b86908f8f93775d4e17f48a
dongcao0307 <caothanhdong.41118@gmail.com>
2024-10-24 00:21:03 +0700

    add DoiPhong_FORM

---
+++ src/form/DoiPhong_FORM.java
@@ -0,0 +1,12 @@
+package form;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class DoiPhong_FORM extends JPanel {
+    public DoiPhong_FORM() {
+        setLayout(new BorderLayout());
+        setBackground(new Color(16, 16, 20));
+
+    }
+}

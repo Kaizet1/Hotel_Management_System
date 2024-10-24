ae4b1857c2296a94bad2533b422df83ac9a0675f
dongcao0307 <caothanhdong.41118@gmail.com>
2024-10-25 01:14:07 +0700

    add CapNhatNhanVien_FORM

---
+++ src/form/CapNhatNhanVien_FORM.java
@@ -0,0 +1,11 @@
+package form;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CapNhatNhanVien_FORM extends JPanel {
+    public CapNhatNhanVien_FORM() {
+        setLayout(new BorderLayout());
+        setBackground(new Color(16, 16, 20));
+    }
+}
